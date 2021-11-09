541a97fca6eed94358722acbc3f44d26a0003d21
大丈夫yao <59105682+Yao-Joe@users.noreply.github.com>
2021-11-09 21:48:49 +0800

    Add files via upload

---
+++ Eg2.py
@@ -0,0 +1,22 @@
+# lst = []
+#
+# for _ in range(5):
+#     goods = input("Please input good num and good name")
+#     lst.append(goods)
+#
+# for item in lst:
+#     print(item)
+#
+# cart = []
+# while True:
+#     num = input("你想输入的商品编号")
+#     for item in lst:
+#         if item.find(num)!=-1:
+#             cart.append(item)
+#             break
+#     if num == 'q':
+#         break
+#
+# for _ in cart:
+#     print(_)
+
